dc797b0ac96a50615fa6ca9e3b1fa7344578ccfc
Angela Zhou <angelazhou922@gmail.com>
2018-12-07 12:22:04 -0800

    buttons

--- challenge_3/client/src/components/ScorePad.jsx
+++ challenge_3/client/src/components/ScorePad.jsx
@@ -14,14 +14,51 @@ class ScorePad extends React.Component {
         seven: false,
         eight: false,
         nine: false,
-        ten: false
+        ten: false,
+        keypad: 'red'
     }
+  }
+
+  keepScore(){
+
   }
 
   render() {
     return (
-      <div>
-        <div className='keypad'>{this.state.one} 1</div>
+      <div className="keypad">
+          <button name="1" style={{color: this.state.keypad}}>
+            1 {this.state.one}
+          </button>
+          <button name="2" style={{color: this.state.keypad}}>
+            2 {this.state.two}
+          </button>
+          <button name="3" style={{color: this.state.keypad}}>
+            3 {this.state.three}
+          </button>
+          <br />
+          <button name="4" style={{color: this.state.keypad}}>
+            4 {this.state.four}
+          </button>
+          <button name="5" style={{color: this.state.keypad}}>
+            5 {this.state.five}
+          </button>
+          <button name="6" style={{color: this.state.keypad}}>
+            6 {this.state.six}
+          </button>
+          <br />
+          <button name="7" style={{color: this.state.keypad}}>
+            7 {this.state.seven}
+          </button>
+          <button name="8" style={{color: this.state.keypad}}>
+            8 {this.state.eight}
+          </button>
+          <button name="9" style={{color: this.state.keypad}}>
+            9 {this.state.nine}
+          </button>
+          <br />
+          <button name="10" style={{color: this.state.keypad}}>
+            10 {this.state.ten}
+          </button>
       </div>
     )
   }
