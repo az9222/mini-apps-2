df049a253a393b2cbe26c928b5380a6213d016e8
Angela Zhou <angelazhou922@gmail.com>
2018-12-07 11:58:48 -0800

    css

--- challenge_3/client/src/components/ScorePad.jsx
+++ challenge_3/client/src/components/ScorePad.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+// import style from './style.css';
 
 class ScorePad extends React.Component {
   constructor(props) {
     super(props) 
       this.state = {
-
+        one: false,
+        two: false,
+        three: false,
+        four: false,
+        five: false,
+        six: false,
+        seven: false,
+        eight: false,
+        nine: false,
+        ten: false
     }
   }
 
   render() {
     return (
       <div>
-        <p>Hi</p>
+        <div className='keypad'>{this.state.one} 1</div>
       </div>
     )
   }
